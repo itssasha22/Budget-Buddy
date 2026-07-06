f16f015084d9698cf3636641a2501f16e4abb399
Crimzor <your-email@example.com>
2026-07-06 15:46:57 +0300

    update

---
+++ budgetbuddy-backend/app/core/logging.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+import structlog
+
+def setup_logging():
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=logging.INFO,
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer()
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+setup_logging()
+logger = structlog.get_logger()
